de856baefa951c5d36782ccfce13af454f670f86
Dev-Shashwat-Et <53172942+Prodigy-Dev-NK@users.noreply.github.com>
2026-01-22 19:45:58 +0545

    project setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+setup(
+    name="shikshya_sambad",
+    version="0.1.0",
+    author="Shashwat",
+    author_email="kadayatshaswat99@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+
+)
